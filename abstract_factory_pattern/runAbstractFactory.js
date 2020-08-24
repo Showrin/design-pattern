3f0124488b29c28f23ee49477c8c67807fb5a938
Showrin <showrinece15@gmail.com>
2020-08-24 21:30:31 +0600

    Modify runner function to print result in colorful table

--- abstract_factory_pattern/runAbstractFactory.js
+++ abstract_factory_pattern/runAbstractFactory.js
@@ -1,8 +1,25 @@
 "use strict";
 exports.__esModule = true;
 var VehicleFactory_1 = require("./VehicleFactory");
+var console_table_printer_1 = require("console-table-printer");
+var chalk = require("chalk");
 var vehicleFactory = new VehicleFactory_1["default"]();
 var ourCar = vehicleFactory.createCar();
 var ourPlane = vehicleFactory.createPlane();
-console.log(ourCar.set_num_of_wheels(), ourCar.set_num_of_passengers(), ourCar.has_gas());
-console.log(ourPlane.set_num_of_wheels(), ourPlane.set_num_of_passengers(), ourPlane.has_gas());
+var table = new console_table_printer_1.Table();
+table.addRow({
+    Product: "Car",
+    set_num_of_wheels: ourCar.set_num_of_wheels(),
+    set_num_of_passengers: ourCar.set_num_of_passengers(),
+    has_gas: ourCar.has_gas()
+}, { color: "red" });
+table.addRow({
+    Product: "Plane",
+    set_num_of_wheels: ourPlane.set_num_of_wheels(),
+    set_num_of_passengers: ourPlane.set_num_of_passengers(),
+    has_gas: ourPlane.has_gas()
+}, { color: "green" });
+console.log(chalk.bold.red("\n### These products are created using [" +
+    chalk.underline("Abstract Factory Pattern") +
+    "] ###"));
+table.printTable();
--- factory_pattern/runFactory.ts
+++ factory_pattern/runFactory.ts
@@ -1,19 +1,41 @@
 import VehicleFactory from "./VehicleFactory";
 import Car from "./Car";
 import Plane from "./Plane";
+import { Table } from "console-table-printer";
+const chalk = require("chalk");
 
 const vehicleFactory: VehicleFactory = new VehicleFactory();
 const ourCar: Car = vehicleFactory.getCar();
 const ourPlane: Plane = vehicleFactory.getPlane();
 
-console.log(
-  ourCar.set_num_of_wheels(),
-  ourCar.set_num_of_passengers(),
-  ourCar.has_gas()
+const table = new Table();
+
+table.addRow(
+  {
+    Product: "Car",
+    set_num_of_wheels: ourCar.set_num_of_wheels(),
+    set_num_of_passengers: ourCar.set_num_of_passengers(),
+    has_gas: ourCar.has_gas(),
+  },
+  { color: "red" }
+);
+
+table.addRow(
+  {
+    Product: "Plane",
+    set_num_of_wheels: ourPlane.set_num_of_wheels(),
+    set_num_of_passengers: ourPlane.set_num_of_passengers(),
+    has_gas: ourPlane.has_gas(),
+  },
+  { color: "green" }
 );
 
 console.log(
-  ourPlane.set_num_of_wheels(),
-  ourPlane.set_num_of_passengers(),
-  ourPlane.has_gas()
+  chalk.bold.red(
+    "\n### These products are created using [" +
+      chalk.underline("Factory Pattern") +
+      "] ###"
+  )
 );
+
+table.printTable();
